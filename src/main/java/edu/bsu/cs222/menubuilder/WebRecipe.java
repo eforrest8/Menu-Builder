01c06fae3c8f21b6ddda0d3a681d9b24458efcd8
Ethan Forrest <ethan.forrest@bsu.edu>
2022-03-17 16:24:06 -0400

    Add WebRecipe class

---
+++ src/main/java/edu/bsu/cs222/menubuilder/WebRecipe.java
@@ -0,0 +1,13 @@
+package edu.bsu.cs222.menubuilder;
+
+public class WebRecipe implements Recipe {
+    @Override
+    public String recipeText() {
+        return null;
+    }
+
+    @Override
+    public List<Ingredient> ingredients() {
+        return null;
+    }
+}
