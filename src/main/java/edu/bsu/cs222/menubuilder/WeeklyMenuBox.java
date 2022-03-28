a64bf403bb9268caf5b70dcba3f51e00ed660ad3
Ethan Forrest <ethan.forrest@bsu.edu>
2022-03-28 12:24:18 -0400

    Move size configuration to buildUI()
    Probably needs to go in its own method in the long run.

--- src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
+++ src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
@@ -6,10 +6,11 @@ public class WeeklyMenuBox extends HBox {
     private Menu menu;
     public WeeklyMenuBox(Menu menu) {
         this.menu = menu;
-        this.setMinSize(640, 480);
+        this.buildUI();
     }
 
     private void buildUI() {
+        this.setMinSize(640, 480);
         for (Day day:
                 menu) {
             this.getChildren().add(new DayView(day));
