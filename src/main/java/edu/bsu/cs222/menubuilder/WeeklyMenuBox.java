95302a918ea1906a4930d93fad663367930086cb
Ethan <ethan.forrest@bsu.edu>
2022-03-28 11:54:50 -0400

    Create WeeklyMenuBox

--- src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
+++ src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
@@ -3,6 +3,7 @@ package edu.bsu.cs222.menubuilder;
 import javafx.scene.layout.HBox;
 
 public class WeeklyMenuBox extends HBox {
+    Day[] days = new Day[7];
     public WeeklyMenuBox(Menu menu) {
 
     }
