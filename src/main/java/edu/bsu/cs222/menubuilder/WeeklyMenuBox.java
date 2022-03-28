7a7e90a8f94e1c8ca96072a6425a3b6db3a3bfa6
Ethan Forrest <ethan.forrest@bsu.edu>
2022-03-28 12:21:01 -0400

    Implement WeeklyMenuBox
    Basic naive implementation. No metadata yet, it's just an
    HBox of DayViews.

--- src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
+++ src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuBox.java
@@ -3,8 +3,16 @@ package edu.bsu.cs222.menubuilder;
 import javafx.scene.layout.HBox;
 
 public class WeeklyMenuBox extends HBox {
-    WeekDay[] weekDays = new WeekDay[7];
+    private Menu menu;
     public WeeklyMenuBox(Menu menu) {
+        this.menu = menu;
+        this.setMinSize(640, 480);
+    }
 
+    private void buildUI() {
+        for (Day day:
+                menu) {
+            this.getChildren().add(new DayView(day));
+        }
     }
 }
