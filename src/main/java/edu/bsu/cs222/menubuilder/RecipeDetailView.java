9b58f471bd09b246a66e0e93a37f1543478e730b
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-07 16:45:08 -0400

    Refactor RecipeDetailView.java

--- src/main/java/edu/bsu/cs222/menubuilder/RecipeDetailView.java
+++ src/main/java/edu/bsu/cs222/menubuilder/RecipeDetailView.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import org.w3c.dom.Document;
 
 import java.awt.*;
 import java.io.IOException;
@@ -14,6 +13,7 @@ import java.util.Locale;
 
 
 public class RecipeDetailView extends VBox {
+
     public RecipeDetailView(Recipe recipe) {
         this.getChildren().addAll(
                 getTitleLabel(recipe),
@@ -28,28 +28,32 @@ public class RecipeDetailView extends VBox {
     private Node getRecipeLocation(Recipe recipe) {
         if (recipe instanceof RemoteRecipe remoteRecipe) {
             Button button = new Button("View recipe in browser");
-            button.setOnAction(e -> handleLink(remoteRecipe.getURI()));
+            button.setOnAction(e -> openBrowserToURI(remoteRecipe.getURI()));
             return button;
         }
         return new Label("test label");
     }
 
-    private void handleLink(URI uri) {
+    private void openBrowserToURI(URI uri) {
         try {
             if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("linux")) {
-                // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
-                if (Runtime.getRuntime().exec(new String[] { "which", "xdg-open" }).getInputStream().read() != -1) {
-                    Runtime.getRuntime().exec(new String[] { "xdg-open", uri.toString() });
-                } else {
-                    new Alert(Alert.AlertType.ERROR, "xdg-open not supported!").show();
-                }
+                openBrowserOnLinux(uri);
             } else if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
                 Desktop.getDesktop().browse(uri);
             } else {
-                throw new UnsupportedOperationException("Opening a browser is not supported.");
+                throw new UnsupportedOperationException("Opening a web browser is not supported by your system.");
             }
         } catch (IOException | UnsupportedOperationException exception) {
-            new Alert(Alert.AlertType.ERROR, "Unable to launch browser").show();
+            new Alert(Alert.AlertType.ERROR, "Encountered exception while trying to open web browser.\n" + exception.getLocalizedMessage()).show();
+        }
+    }
+
+    private void openBrowserOnLinux(URI uri) throws IOException {
+        // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+        if (Runtime.getRuntime().exec(new String[] { "which", "xdg-open" }).getInputStream().read() != -1) {
+            Runtime.getRuntime().exec(new String[] { "xdg-open", uri.toString() });
+        } else {
+            throw new UnsupportedOperationException("Can't launch default web browser; xdg-open not supported!");
         }
     }
 }
