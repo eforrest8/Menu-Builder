97c7de900beea6a842a2d96b4abb4973cd22a629
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-07 21:18:14 -0400

    Create WeeklyMenuEditorGroup
    This might have been a replacement of another class, I
    forget. In any case, it's nearly the same as
    WeeklyMenuViewGroup. I did combine the separate button
    builders into one; three arguments feels like a lot,
    though, so that's a good candidate for refactoring later.

---
+++ src/main/java/edu/bsu/cs222/menubuilder/WeeklyMenuEditorGroup.java
@@ -0,0 +1,68 @@
+package edu.bsu.cs222.menubuilder;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class WeeklyMenuEditorGroup extends Group {
+
+    private final Menu initialMenu;
+    private final Menu editedMenu;
+
+    public WeeklyMenuEditorGroup(Menu menu) {
+        initialMenu = menu;
+        editedMenu = menu;
+        buildUI(menu);
+    }
+
+    private void buildUI(Menu menu) {
+        HBox box = configureHBox();
+        for (Day day: menu) {
+            box.getChildren().add(new DayEditorBox(day));
+        }
+        this.getChildren().add(new VBox(buildButtonBar(), box));
+    }
+
+    private ButtonBar buildButtonBar() {
+        ButtonBar bar = new ButtonBar();
+        bar.getButtons().addAll(
+                buildButton("Save & Close", ButtonBar.ButtonData.APPLY, this::saveAndClose),
+                buildButton("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE,
+                        (e) -> this.getScene().setRoot(new WeeklyMenuViewGroup(initialMenu)))
+        );
+        return bar;
+    }
+
+    private void saveAndClose(ActionEvent event) {
+        this.getScene().setRoot(new WeeklyMenuViewGroup(editedMenu));
+    }
+
+    private Button buildButton(String text, ButtonBar.ButtonData buttonData, EventHandler<ActionEvent> onAction) {
+        Button button = new Button(text);
+        ButtonBar.setButtonData(button, buttonData);
+        button.setOnAction(onAction);
+        return button;
+    }
+
+    private void chooseMenuFile() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Open Menu File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Json Files", "*.json")
+        );
+        File result = chooser.showOpenDialog(this.getScene().getWindow());
+    }
+
+    private static HBox configureHBox() {
+        HBox box = new HBox();
+        box.setMinSize(640, 480);
+        return box;
+    }
+}
