93e1ed039b8dbce57131598342e18d02d763f5ee
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-09 00:48:32 -0400

    Stub EdamamApiProvider
    Just setting the search method to return a list of valid
    urls with titles for UI testing purposes.

--- src/main/java/edu/bsu/cs222/menubuilder/EdamamApiProvider.java
+++ src/main/java/edu/bsu/cs222/menubuilder/EdamamApiProvider.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class EdamamApiProvider implements RecipeProvider {
     @Override
     public List<Recipe> search(String query) {
-        return List.of(new WebRecipe("google", "https://google.com"));
+        return List.of(
+                new WebRecipe("google", "https://google.com"),
+                new WebRecipe("binf", "https://bing.com"),
+                new WebRecipe("has the large hadron collider destroyed the world yet?",
+                        "http://hasthelargehadroncolliderdestroyedtheworldyet.com/")
+                );
     }
 }
