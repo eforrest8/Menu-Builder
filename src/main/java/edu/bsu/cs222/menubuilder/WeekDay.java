f1f5971ac2d8086d94b73808eb51d967a3a3ac90
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-10 04:40:24 -0400

    Import cleanup

--- src/main/java/edu/bsu/cs222/menubuilder/WeekDay.java
+++ src/main/java/edu/bsu/cs222/menubuilder/WeekDay.java
@@ -1,11 +1,8 @@
 package edu.bsu.cs222.menubuilder;
 
 import java.time.DayOfWeek;
-import java.time.format.TextStyle;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 
 public record WeekDay(DayOfWeek dayOfWeek, List<WebRecipe> recipes) {
     public WeekDay(DayOfWeek dayOfWeek) {
