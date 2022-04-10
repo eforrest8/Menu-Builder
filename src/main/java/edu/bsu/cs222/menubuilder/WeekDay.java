5512d0a3ccb009ce59d951573ffd6f007a99b01a
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-10 01:43:17 -0400

    Add generated equals method
    Necessary for the current json generation method.

--- src/main/java/edu/bsu/cs222/menubuilder/WeekDay.java
+++ src/main/java/edu/bsu/cs222/menubuilder/WeekDay.java
@@ -2,6 +2,7 @@ package edu.bsu.cs222.menubuilder;
 
 import java.time.DayOfWeek;
 import java.util.LinkedList;
+import java.util.Objects;
 
 public class WeekDay extends LinkedList<WebRecipe> implements Day {
     private final DayOfWeek dayOfWeek;
@@ -17,5 +18,19 @@ public class WeekDay extends LinkedList<WebRecipe> implements Day {
     @Override
     public int getIndex() {
         return dayOfWeek.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        WeekDay that = (WeekDay) o;
+        return dayOfWeek == that.dayOfWeek;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), dayOfWeek);
     }
 }
