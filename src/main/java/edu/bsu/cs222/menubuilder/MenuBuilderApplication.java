51fe62da72c9fabaf17734c46dcc5614cf6d09bf
Ethan Forrest <ethan.forrest@bsu.edu>
2022-04-10 01:24:22 -0400

    Add save prompt when closing the application

--- src/main/java/edu/bsu/cs222/menubuilder/MenuBuilderApplication.java
+++ src/main/java/edu/bsu/cs222/menubuilder/MenuBuilderApplication.java
@@ -1,20 +1,34 @@
 package edu.bsu.cs222.menubuilder;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class MenuBuilderApplication extends Application {
 
+    private Stage stage;
+
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setScene(buildUI());
-        primaryStage.show();
+        stage = primaryStage;
+        stage.setScene(buildUI());
+        stage.setOnCloseRequest(this::handleCloseRequest);
+        stage.show();
+    }
+
+    private void handleCloseRequest(WindowEvent windowEvent) {
+        if (windowEvent.getEventType() != WindowEvent.WINDOW_CLOSE_REQUEST) {
+            return;
+        }
+        MenuFileManager fileManager = new MenuFileManager();
+        fileManager.saveFile(stage.getOwner());
+        Platform.exit();
     }
 
     private Scene buildUI() {
-        Menu menu = new WeeklyMenu();
-        return new Scene(new WeeklyMenuViewGroup(menu));
+        return new Scene(new WeeklyMenuViewGroup());
     }
 
 }
