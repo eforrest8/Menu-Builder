0e73f1bd97af5e8a230f20473d803bc39c3c26ff
eforrest8 <9276478+eforrest8@users.noreply.github.com>
2022-04-16 16:46:34 -0400

    Extract browser-launching code into standalone class
    I'm not 100% on the static methods in here, but
    Desktop.getDesktop().browse() is a static method call
    (or at least getDesktop() is.) And I don't expect
    polymorphism to be a problem.
    
    That said, we could move to a polymorphic form; maybe a
    WindowsBrowserLauncher and a LinuxBrowserLauncher? feels
    a little overkill though.

---
+++ src/main/java/edu/bsu/cs222/menubuilder/BrowserLauncher.java
@@ -0,0 +1,27 @@
+package edu.bsu.cs222.menubuilder;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Locale;
+
+class BrowserLauncher {
+    public static void openBrowserToURI(URI uri) throws IOException {
+        if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("linux")) {
+            openBrowserOnLinux(uri);
+        } else if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            Desktop.getDesktop().browse(uri);
+        } else {
+            throw new IOException("Opening a web browser is not supported by your system.");
+        }
+    }
+
+    public static void openBrowserOnLinux(URI uri) throws IOException {
+        // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
+        if (Runtime.getRuntime().exec(new String[]{"which", "xdg-open"}).getInputStream().read() != -1) {
+            Runtime.getRuntime().exec(new String[]{"xdg-open", uri.toString()});
+        } else {
+            throw new IOException("Can't launch default web browser; xdg-open not supported!");
+        }
+    }
+}
--- src/main/java/edu/bsu/cs222/menubuilder/RecipeViewBox.java
+++ src/main/java/edu/bsu/cs222/menubuilder/RecipeViewBox.java
@@ -7,10 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.OverrunStyle;
 import javafx.scene.layout.VBox;
 
-import java.awt.*;
 import java.io.IOException;
-import java.net.URI;
-import java.util.Locale;
 
 
 public class RecipeViewBox extends VBox {
@@ -32,30 +29,16 @@ public class RecipeViewBox extends VBox {
 
     private Node getRecipeLocation(WebRecipe recipe) {
         Button button = new Button("View Recipe");
-        button.setOnAction(e -> openBrowserToURI(recipe.getURI()));
+        button.setOnAction(e -> {
+            try {
+                BrowserLauncher.openBrowserToURI(recipe.getURI());
+            } catch (IOException exception) {
+                new Alert(Alert.AlertType.ERROR,
+                        "Encountered exception while trying to open web browser.\n" +
+                                exception.getLocalizedMessage()).show();
+            }
+        });
         return button;
     }
 
-    private void openBrowserToURI(URI uri) {
-        try {
-            if (System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("linux")) {
-                openBrowserOnLinux(uri);
-            } else if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-                Desktop.getDesktop().browse(uri);
-            } else {
-                throw new UnsupportedOperationException("Opening a web browser is not supported by your system.");
-            }
-        } catch (IOException | UnsupportedOperationException exception) {
-            new Alert(Alert.AlertType.ERROR, "Encountered exception while trying to open web browser.\n" + exception.getLocalizedMessage()).show();
-        }
-    }
-
-    private void openBrowserOnLinux(URI uri) throws IOException {
-        // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't work on some Linux implementations
-        if (Runtime.getRuntime().exec(new String[] { "which", "xdg-open" }).getInputStream().read() != -1) {
-            Runtime.getRuntime().exec(new String[] { "xdg-open", uri.toString() });
-        } else {
-            throw new UnsupportedOperationException("Can't launch default web browser; xdg-open not supported!");
-        }
-    }
 }
